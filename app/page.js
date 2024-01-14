6a18d5673b638dbf878c9c3ea54f20aecceed935
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-01-14 23:03:42 +0530

    Add new files and configurations

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { ArrowCircleDown } from '@phosphor-icons/react/dist/ssr'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex h-screen w-screen justify-center items-center'>
+      <div className='bg-foreground rounded-2xl w-3/4 lg:w-1/2 h-5/6 lg:h-3/4'>
+
+        <div></div>
+
+      </div>
+    </main>
+  )
+}
