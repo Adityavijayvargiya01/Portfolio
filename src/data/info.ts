7c4a9c1772052b9fe6a809cb9d613bef34cbdde8
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-02-05 23:57:21 +0530

    New Desc 1.0

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,15 +2,7 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Aditya Vijayvargiya",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Pune, India.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
-  `,
+  about: `I'm a software engineer based in Pune, India, with 3 years of experience in front-end development. I specialize in Next.js, React, Tailwind CSS, and Astro, with skills in Node.js and Express.js. I've completed numerous web projects from scratch and continue to explore new technologies like React Native and Firebase.`,
 
   experience: [
     
